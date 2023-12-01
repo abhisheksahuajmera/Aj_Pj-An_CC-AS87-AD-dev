92f7176b570f67a73554695abdd32e066b195581
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-01 18:55:30 +0530

    Try 106

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "729672c5-d8c3-49fd-be05-c404781c9394"
