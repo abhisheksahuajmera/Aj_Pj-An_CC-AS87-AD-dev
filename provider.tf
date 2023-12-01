8d2c8f5bfe224279e3d34402490f2ea2a8a21714
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-01 16:54:35 +0530

    Resolve 100-

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 // Store in Git->Connect throgh vcs->Connect Azure using Tera Cloud Env Var->run mannually from new run->implement in azure
 terraform {
   cloud {
-    organization = "${var.org}"
+    organization = "Ajmerainfotech" //"${var.org}"
 
     workspaces {
-      name = "${var.workspace}"
+      name = "Aj_Pj-An_CC-AS87-AD-dev" //"${var.workspace}"
     }
   }
 }
