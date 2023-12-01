0fb16421edcceca68965fd5706319cb9ff47033f
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-01 19:06:09 +0530

    Try 107

--- provider.tf
+++ provider.tf
@@ -1,17 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = "729672c5-d8c3-49fd-be05-c404781c9394"
-  client_id       = "c1d00aec-4e92-4fae-88a7-1d68a129b074"
-  client_secret   = "o1b8Q~4b3Igq_EGwggO4SOjoOtv5u6jSEDrQWccn"
-  tenant_id       = "593fcce9-75cb-472c-9784-f4ae4637b2fb"
+	backend "azurerm" {
+	}
 }
