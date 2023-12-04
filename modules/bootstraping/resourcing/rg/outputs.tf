1f8833837340e48ad0406315497a3174cb42ed98
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 18:44:09 +0530

    Resolve 600

--- modules/bootstraping/resourcing/rg/outputs.tf
+++ modules/bootstraping/resourcing/rg/outputs.tf
@@ -1,2 +1,6 @@
 # output "nameop"      { value = "${azurerm_resource_group.resource_group.name}" }
 # output "locationop"      { value = "${azurerm_resource_group.resource_group.location}" }
+
+
+output "resource_group_nameop"      { value = local.resource_group_name }
+output "resource_group_locationop"      { value = local.resource_group_location }
