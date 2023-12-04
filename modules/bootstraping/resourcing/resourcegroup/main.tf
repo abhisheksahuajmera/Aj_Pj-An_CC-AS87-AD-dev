9ea662e0cb3ead7728ec208ccf746bd47fcbe756
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 18:06:07 +0530

    Resolve 302

--- modules/bootstraping/resourcing/resourcegroup/main.tf
+++ modules/bootstraping/resourcing/resourcegroup/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "azurerm_resource_grouptg" {
+resource "azurerm_resource_group" "azurerm_resource_group" {
   name     = local.resource_group_name
   location = local.resource_group_location
 }
