4fc7d8ae42838cbd3442e4615fb3cd30e0311a41
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 19:06:54 +0530

    Resolve 002

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,3 @@ output "projectop" { value = var.project }
 output "prefixop" { value = var.prefix }
 output "environmentop" { value = var.environment }
 output "environment_versionop" { value = var.environment_version }
-output "resource_group_name " { value = local.resource_group_name }
