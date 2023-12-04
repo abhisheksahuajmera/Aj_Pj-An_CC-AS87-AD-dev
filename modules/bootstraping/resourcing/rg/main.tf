30c1aca5559ab3e1c876838e9e5c03f952b2834c
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 18:15:07 +0530

    Resolve 308

--- modules/bootstraping/resourcing/rg/main.tf
+++ modules/bootstraping/resourcing/rg/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "azurerm_resource_group" {
+resource "azurerm_resource_group" "azurerm_resource_grouptg" {
   name     = local.resource_group_name
   location = local.resource_group_location
 }
--- modules/bootstraping/resourcing/rg/outputs.tf
+++ modules/bootstraping/resourcing/rg/outputs.tf
@@ -3,5 +3,6 @@
 # output "resource_group_country_code"      { value = local.resource_group_country_code }
 
 
-output "nameop"      { value = "${azurerm_resource_group.azurerm_resource_grouptg.name}" }
-output "locationop"      { value = "${azurerm_resource_group.azurerm_resource_grouptg.location}" }
+output "resource_group_name"      { value = "${azurerm_resource_group.azurerm_resource_grouptg.name}" }
+output "resource_group_location"      { value = "${azurerm_resource_group.azurerm_resource_grouptg.location}" }
+output "resource_group_country_code"      { value = "${azurerm_resource_group.azurerm_resource_grouptg.resource_group_country_code}" }
