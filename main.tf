d2bf0b3cca5392f5ece7ddc3341e56ae199a5a5e
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 18:33:06 +0530

    Resovle 502

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+// bootstraping-resourcing
 module "resourcing_rg" {
   source              = "./modules/bootstraping/resourcing/rg" // Add version after registry
   prefix              = var.prefix
   environment         = var.environment
   environment_version = var.environment_version
+}
+
+// appusing-securing-adb2c
+module "securing_adb2c" {
+  source              = "./modules/appusing/securing/adb2c" // Add version after registry
 }
