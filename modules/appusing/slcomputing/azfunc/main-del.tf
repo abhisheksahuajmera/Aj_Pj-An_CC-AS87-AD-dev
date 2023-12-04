bd045fe7cd21890e7edc78c03827239d917fffd0
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 16:52:20 +0530

    Try Azure Func 100

--- modules/appusing/slcomputing/azfunc/main-del.tf
+++ modules/appusing/slcomputing/azfunc/main-del.tf
@@ -1,20 +1,26 @@
-resource "azurerm_resource_group" "example" {
-  name     = "azure-functions-test-rg"
-  location = "West Europe"
-}
+# resource "azurerm_resource_group" "example" {
+#   name     = "azure-functions-test-rg"
+#   location = "West Europe"
+# }
 
 resource "azurerm_storage_account" "example" {
   name                     = "functionsapptestsa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = "${modules.resourcing.resource_group_name}"
+  location                 = "${modules.resourcing.resource_group_location}"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
+  data_residency_location = "${modules.resourcing.resource_group_location}"
+  display_name            = local.display_name
+  domain_name             = local.domain_name
+  resource_group_name     = "${modules.resourcing.resource_group_name}"
+
+
 resource "azurerm_app_service_plan" "example" {
   name                = "azure-functions-test-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = "${modules.resourcing.resource_group_location}"
+  resource_group_name = "${modules.resourcing.resource_group_name}"
 
   sku {
     tier = "Standard"
@@ -24,8 +30,8 @@ resource "azurerm_app_service_plan" "example" {
 
 resource "azurerm_function_app" "example" {
   name                       = "test-azure-functions"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
+  location                   = "${modules.resourcing.resource_group_location}"
+  resource_group_name        = "${modules.resourcing.resource_group_name}"
   app_service_plan_id        = azurerm_app_service_plan.example.id
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
