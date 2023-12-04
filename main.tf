b249d3b7caa953e2590815306fcde9a1dfcddb93
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 19:11:07 +0530

    Resolve 103

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "securing_adb2c" {
   source                  = "./modules/adb2c" // Add version after registry
   resource_group_name     = local.resource_group_name
   resource_group_location = local.resource_group_location
-  resource_group_location = local.resource_group_location_country_code
+  resource_group_location_country_code = local.resource_group_location_country_code
 }
 
 // bootstraping-resourcing-rg
