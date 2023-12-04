5d197972efdbe92d86c11c58edb9beb07d76847f
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 18:30:50 +0530

    Resolve 501

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "resourcing" {
+module "resourcing_rg" {
   source              = "./modules/bootstraping/resourcing/rg" // Add version after registry
   prefix              = var.prefix
   environment         = var.environment
--- modules/securing/adb2c/main.tf
+++ modules/securing/adb2c/main.tf
@@ -1,7 +1,7 @@
 // Resource
 resource "azurerm_aadb2c_directory" "example2" {
   country_code            = "US"
-  data_residency_location = "${modules.resourcing.resource_group_location}" //"east us" 
+  data_residency_location = "${modules.resourcing_rg.resource_group_location}" //"east us" 
   display_name            = "example-b2c-tenant"
   domain_name             = "exampleb2ctenant.onmicrosoft.com"
   resource_group_name     = "Assetronai-dev-100-rg-100"
