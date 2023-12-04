0cb5c6cd22a40731861483b80c9c0557d6bebd99
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 17:43:05 +0530

    Resolve 203

--- modules/appusing/securing/adb2c/main.tf
+++ modules/appusing/securing/adb2c/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_aadb2c_directory" "azurerm_aadb2c_directorytg" {
-  country_code            = "${module.resourcing_resourcegroup.resource_group_country_code}"
+  country_code            = "${module.resource_grouptg.resource_group_country_code}"
   data_residency_location = "${module.resourcing_resourcegroup.resource_group_location}"
   display_name            = local.display_name
   domain_name             = local.domain_name
