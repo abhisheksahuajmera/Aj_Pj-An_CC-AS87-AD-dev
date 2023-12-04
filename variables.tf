815155a7390fcce07b76c9da238f32c50c577530
Abhishek Sahu <abhishek.sahu@ajmerainfotech.com>
2023-12-04 20:03:09 +0530

    Resolve 100

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment_version" {
 variable "resource_group_version" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "101"
+  default     = "100"
 }
 
 variable "resource_group_prefix" {
