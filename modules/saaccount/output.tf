6af63d0becf4e3485d5412ba7adb405b8a97e891
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-05 19:50:14 +0530

    Try 103

--- modules/saaccount/output.tf
+++ modules/saaccount/output.tf
@@ -1,6 +1,6 @@
 output "storage_account_nameop" {
   description = "Default hostname of the created Function App"
-  value       = local.name
+  value       = azurerm_storage_account.azurerm_storage_accounttg.name
 }
 
 output "storage_account_access_keyop" {
