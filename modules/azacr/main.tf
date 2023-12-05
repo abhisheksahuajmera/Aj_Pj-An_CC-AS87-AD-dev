ad34eb8c672b2bfdcbc7effb04b67dcb11df7e4f
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-05 19:26:40 +0530

    Try 100

--- modules/azacr/main.tf
+++ modules/azacr/main.tf
@@ -1,22 +1,22 @@
-# resource "azurerm_container_registry" "azurerm_container_registrytg" {
-#   name                = local.azacr_name
-#   resource_group_name = "${var.resource_group_name}"
-#   location            = "${var.location}"
-#   sku                 = "${var.azacr_sku}"
-#   admin_enabled       = "${var.azacr_admin_enabled}"
-# }
+resource "azurerm_container_registry" "azurerm_container_registrytg" {
+  name                = local.azacr_name
+  resource_group_name = "${var.resource_group_name}"
+  location            = "${var.location}"
+  sku                 = "${var.azacr_sku}"
+  admin_enabled       = "${var.azacr_admin_enabled}"
+}
 
-# resource "azurerm_container_registry_webhook" "azurerm_container_registry_webhooktg" {
-#   name                = local.azacrwh_name
-#   resource_group_name = "${var.resource_group_name}"
-#   registry_name       = azurerm_container_registry.azurerm_container_registrytg.name
-#   location            = "${var.location}"
+resource "azurerm_container_registry_webhook" "azurerm_container_registry_webhooktg" {
+  name                = local.azacrwh_name
+  resource_group_name = "${var.resource_group_name}"
+  registry_name       = azurerm_container_registry.azurerm_container_registrytg.name
+  location            = "${var.location}"
 
-#   service_uri = "${var.azacrwh_service_uri}"
-#   status      = "${var.azacrwh_status}"
-#   scope       = "${var.azacrwh_scope}"
-#   actions     = ["${var.azacrwh_actions}"]
-#   custom_headers = {
-#     "Content-Type" = "${var.azacrwh_custom_headers_content_type}"
-#   }
-# }
+  service_uri = "${var.azacrwh_service_uri}"
+  status      = "${var.azacrwh_status}"
+  scope       = "${var.azacrwh_scope}"
+  actions     = ["${var.azacrwh_actions}"]
+  custom_headers = {
+    "Content-Type" = "${var.azacrwh_custom_headers_content_type}"
+  }
+}
