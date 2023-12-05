64f63458b5d96a538909e8029cc768ae7b39e8f7
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-05 19:52:42 +0530

    Try All 100

--- variables.tf
+++ variables.tf
@@ -37,7 +37,7 @@ variable "environment_version" {
 variable "resource_group_version" {
   type        = string
   description = "(Required) Update to force a change."
-  default     = "107"
+  default     = "108"
 }
 
 variable "resource_group_prefix" {
