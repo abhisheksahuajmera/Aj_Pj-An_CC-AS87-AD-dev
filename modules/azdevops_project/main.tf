6cecfeba1f363974beb218741de7010d8db53670
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-07 17:22:41 +0530

    Try 101

--- modules/azdevops_project/main.tf
+++ modules/azdevops_project/main.tf
@@ -1,22 +1,15 @@
-# terraform {
-#   required_providers {
-#     azuredevops = {
-#       source = "microsoft/azuredevops"
-#       version = "0.1.7"
-#     }
-#   }
-# }
-
-# provider "azuredevops" {
-#   org_service_url = "https://dev.azure.com/ajmerainfotech100"
-#   personal_access_token = "vlij5prwhkclld6zn34l4htqzfjj4onr2qg4jjx37wchr5rgj2fa"
-# }
+terraform {
+  required_providers {
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = "0.1.7"
+    }
+  }
+}
 
 provider "azuredevops" {
-  version = ">= 0.0.1"
-  # Remember to specify the org service url and personal access token details below
   org_service_url = "https://dev.azure.com/ajmerainfotech100"
-  personal_access_token = "vlij5prwhkclld6zn34l4htqzfjj4onr2qg4jjx37wchr5rgj2fa"
+  personal_access_token = "m3qjeqshcosgzfkwy5rervbm276vu5jyjryc3jb6uim3wxtqx63a"
 }
 
 resource "azuredevops_project" "azuredevops_projecttg" {
