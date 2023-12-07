e918f7f8efe1305aeb27dcd7adfc5d042c067aff
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-07 15:38:36 +0530

    Resolve 100

--- modules/azdevops_project/main.tf
+++ modules/azdevops_project/main.tf
@@ -1,9 +1,3 @@
-provider "azuredevops" {
-  version = ">= 0.0.1"
-  org_service_url = "https://dev.azure.com/ajmerainfotech100"
-  personal_access_token = "4x4qccuoumcb2iw4pkkwjqqodulscfaxn4thhj3sfjdvn57xk7ea"
-}
-
 resource "azuredevops_project" "azuredevops_projecttg" {
   project_name       = local.project_name
   description        = var.description
