ad6a82413d35e59a6271d28ae58a06fdda3bf517
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-07 17:53:01 +0530

    Try 101

--- modules/azdevops_project/main.tf
+++ modules/azdevops_project/main.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     azuredevops = {
-#       source = "microsoft/azuredevops"
-#       version = "0.1.7"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = "0.1.7"
+    }
+  }
+}
 
 provider "azuredevops" {
   org_service_url = "https://dev.azure.com/ajmerainfotech100"
