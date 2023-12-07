e5f3520ea6c4664003a88c71f516e71348aaa1d8
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-07 17:52:03 +0530

    Try 112

--- modules/azdevops_project/main.tf
+++ modules/azdevops_project/main.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-    azuredevops = {
-      source = "microsoft/azuredevops"
-      version = "0.1.7"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azuredevops = {
+#       source = "microsoft/azuredevops"
+#       version = "0.1.7"
+#     }
+#   }
+# }
 
 provider "azuredevops" {
   org_service_url = "https://dev.azure.com/ajmerainfotech100"
-  personal_access_token = "776d52ebmag334pme6mnsl25iwawsl3cprivcwcgxa4rjmiky3aa"
+  # personal_access_token = "776d52ebmag334pme6mnsl25iwawsl3cprivcwcgxa4rjmiky3aa"
 }
 
 resource "azuredevops_project" "azuredevops_projecttg" {
