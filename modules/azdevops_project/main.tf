e5fb7e71837b873379658447015f0d704ad38fae
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-07 16:38:02 +0530

    Resolve 101

--- modules/azdevops_project/main.tf
+++ modules/azdevops_project/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "azuredevops" {
   org_service_url = "https://dev.azure.com/ajmerainfotech100"
-  # personal_access_token = "o22rpnsvf2mjwniz5u25f6ndhvnhopse6weniuiclwx4pabtudhq"
+  personal_access_token = "vlij5prwhkclld6zn34l4htqzfjj4onr2qg4jjx37wchr5rgj2fa"
 }
 
 
