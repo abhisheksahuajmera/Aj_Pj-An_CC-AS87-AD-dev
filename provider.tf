350bb9b70fe7032eab58144255a0661ecca4076c
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-07 16:22:17 +0530

    Try 200

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ terraform {
     storage_account_name  = "sa110"
     container_name        = "sacon100"
     key                   = "state100.tfstate" 
-    access_key            = "gM2VSx+n9bCQgm5GEhzMk2r4J8VpEib+3E9ORv+GaI0jATiOgyrK6ZIcaIEMwoy+40llPcFX9Pp/+AStSDJ+Cw=="
+    # access_key            = "gM2VSx+n9bCQgm5GEhzMk2r4J8VpEib+3E9ORv+GaI0jATiOgyrK6ZIcaIEMwoy+40llPcFX9Pp/+AStSDJ+Cw=="
   }
 }
 
@@ -24,8 +24,7 @@ provider "azurerm" {
   features {}
 }
 
-# provider "azuredevops" {
-#   org_service_url = "https://dev.azure.com/ajmerainfotech100"
-#   personal_access_token = "4x4qccuoumcb2iw4pkkwjqqodulscfaxn4thhj3sfjdvn57xk7ea"
-# }
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/ajmerainfotech100"
+}
 
