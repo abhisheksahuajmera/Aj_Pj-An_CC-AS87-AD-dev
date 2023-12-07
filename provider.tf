3f33c96d6a10d164fa84e9e9896135859605611b
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-07 16:07:52 +0530

    Resolve 101

--- provider.tf
+++ provider.tf
@@ -24,3 +24,8 @@ provider "azurerm" {
   features {}
 }
 
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/ajmerainfotech100"
+  personal_access_token = "4x4qccuoumcb2iw4pkkwjqqodulscfaxn4thhj3sfjdvn57xk7ea"
+}
+
