d74fd0c8b1f438eda483498514cb1599c432ce56
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-08 18:08:05 +0530

    Resolve 100

--- provider.tf
+++ provider.tf
@@ -11,14 +11,7 @@ terraform {
     }
   }
 
-  # backend "azurerm" {
-  #   resource_group_name   = "Assetronai-dev-101-rg-108"
-  #   storage_account_name  = "sa110"
-  #   container_name        = "sacon100"
-  #   key                   = "state100.tfstate" 
-  #   access_key            = "gM2VSx+n9bCQgm5GEhzMk2r4J8VpEib+3E9ORv+GaI0jATiOgyrK6ZIcaIEMwoy+40llPcFX9Pp/+AStSDJ+Cw=="
-  # }
-  //QA
+  //Dev
   backend "azurerm" {
     resource_group_name   = "assetronai-dev-100"
     storage_account_name  = "sa712"
