96971422e27ab2a2a6743296688e2ca6f3bf6b68
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-10 14:03:03 +0530

    Try 100

--- backend.hcl
+++ backend.hcl
@@ -12,3 +12,7 @@
 # key                   = var.state_file_name
 
 
+resource_group_name   = var.state_resource_group_name
+storage_account_name  = var.state_storage_account_name
+container_name        = var.state_container_name
+key                   = var.state_key
--- variables.tf
+++ variables.tf
@@ -73,21 +73,25 @@ variable "resource_group_data_residency_location" {
 variable "state_resource_group_name" {
   type        = string
   description = "(Required) Update to force a change."
+  default     = "Assetronai-dev-101-rg-108"
 }
 
 variable "state_storage_account_name" {
   type        = string
   description = "(Required) Update to force a change."
+  default     = "sa110"
 }
 
 variable "state_container_name" {
   type        = string
   description = "(Required) Update to force a change."
+  default     = "sacon100"
 }
 
 variable "state_key" {
   type        = string
   description = "(Required) Update to force a change."
+  default     = "state103.tfstate"
 }
 
 variable "state_file_name" {
