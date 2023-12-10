916fd96f84fcb83902af3ac0fec52690faa2f173
abhisheksahuajmera <150130889+abhisheksahuajmera@users.noreply.github.com>
2023-12-10 13:01:34 +0530

    Try 101

--- provider.tf
+++ provider.tf
@@ -12,13 +12,13 @@ terraform {
   }
 
   //Dev
-  backend "azurerm" {
-    resource_group_name   = "assetronai-dev-100"
-    storage_account_name  = "sa712"
-    container_name        = "sacon101"
-    # key                   = "state103.tfstate"
-    key                   = var.state_file_name
-  }  
+  # backend "azurerm" {
+  #   resource_group_name   = "assetronai-dev-100"
+  #   storage_account_name  = "sa712"
+  #   container_name        = "sacon101"
+  #   # key                   = "state103.tfstate"
+  #   key                   = var.state_file_name
+  # }  
 }
 
 provider "azurerm" {
